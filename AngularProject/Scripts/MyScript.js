f36b1f08fc3b33c5e62638484096d48afa02b81e
GraySerg <grsergaustin@gmail.com>
2015-05-07 12:21:16 +0300

    InitialCommit

---
+++ AngularProject/Scripts/MyScript.js
@@ -0,0 +1,22 @@
+﻿var toDoApp = angular.module("ToDoApp", []);
+var model = { name: "Adam" };
+
+var controler = toDoApp.controller("ToDoController", function ($scope, $http)
+{
+    $http.get("data.json").success(function (res) {
+        model.items = res;
+    });
+    $scope.model = model;
+    $scope.incompleteCount = function () {
+        var count = 0;
+        angular.forEach($scope.model.items, function (item) {
+            if (!item.done) count++;
+        });
+        return count;
+    };
+    $scope.warningLevel = function () {
+        return $scope.incompleteCount() < 3 ? "label-warning" : "label-success";
+    };
+}
+);
+
---
+++ AngularProject/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="angular-mocks.js" />
+/// <reference path="angular.js" />
+/// <reference path="jquery-2.1.3.js" />
+/// <reference path="npm.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="myscript.js" />
