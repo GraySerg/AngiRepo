29b60cd8577197784c5484040f0a3bd5db68d015
Sergey <grsergaustin@gmail.com>
2015-06-03 06:33:11 +0300

    Udate jq

---
+++ AngularProject/Scripts/GitScript.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    var gitApp = angular.module("GitApp", []);
+    var MainController =  function ($scope, $http, $interval, $log, $anchorScroll, $location) {
+        $scope.repoSortOrder = "-stargazers_count";
+        
+        $scope.username = "angular";
+        $scope.search = function (username) {
+            $log.info("Searching for " + username);
+            
+            $http.get("https://api.github.com/users/" + username).success(function (res) {
+                $scope.user = res;
+                $http.get($scope.user.repos_url).success(function (repoList) {
+                    $scope.user.repoList = repoList;
+                    $location.hash("userDetails");
+                    $anchorScroll();//.scroll("userDetails");
+                });
+                if (countDownInterval)
+                {
+                    $interval.cancel(countDownInterval);
+                    $scope.countdown = null;
+                }
+            });
+        };
+        
+        $scope.countdown = 5;
+        var countDownInterval;
+        var decrementCountdown = function () {
+            $scope.countdown -= 1;
+            if ($scope.countdown<1)
+                $scope.search($scope.username);
+
+        };
+        var startCountdown = function()
+        {
+            countDownInterval= $interval(decrementCountdown,1000,$scope.countdown);
+        };
+        startCountdown();
+        
+
+    };
+    
+    gitApp.controller("GitController",MainController);
+}());
+
--- AngularProject/Scripts/MyScript.js
+++ AngularProject/Scripts/MyScript.js
@@ -1,22 +1,25 @@
-﻿var toDoApp = angular.module("ToDoApp", []);
-var model = { name: "Adam" };
+﻿(function () {
+    var toDoApp = angular.module("ToDoApp", []);
+    var model = { name: "Adam" };
 
-var controler = toDoApp.controller("ToDoController", function ($scope, $http)
-{
-    $http.get("data.json").success(function (res) {
-        model.items = res;
-    });
-    $scope.model = model;
-    $scope.incompleteCount = function () {
-        var count = 0;
-        angular.forEach($scope.model.items, function (item) {
-            if (!item.done) count++;
+    var controler = toDoApp.controller("ToDoController", function ($scope, $http) {
+        $http.get("data.json").success(function (res) {
+            model.items = res;
         });
-        return count;
-    };
-    $scope.warningLevel = function () {
-        return $scope.incompleteCount() < 3 ? "label-warning" : "label-success";
-    };
-}
-);
+        $scope.model = model;
+        $scope.incompleteCount = function () {
+            var count = 0;
+            angular.forEach($scope.model.items, function (item) {
+                if (!item.done) count++;
+            });
+            return count;
+        };
+        $scope.warningLevel = function () {
+            return $scope.incompleteCount() < 3 ? "label-warning" : "label-success";
+        };
+    }
+    );
+}());
+
+
 
